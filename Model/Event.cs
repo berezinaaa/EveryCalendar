ddf46ff8d6014ad13626fcd3d7968de86af9fa7d
Anastasia Berezina <berezinaaa@gmail.com>
2016-12-15 01:54:50 +0400

    init commit

---
+++ Model/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum Priority
+    {
+        Low, Middle, High
+    }
+
+    public class Event
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public DateTime Day { get; set; }
+        public Priority Priority { get; set; }
+    }
+}
---
+++ Model/SmsNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace Model
+{
+    public class SmsNotifier: IEventNotifier
+    {
+        public void Notify()
+        {
+            var client = new TwilioRestClient("AC82d123e1c9a93a9e227319c99d3b5072", "a2d72679907147f5d9ab1139f3a1c354");
+            var message = client.SendMessageWithService("MG36acbb3450052e429a34569808ad6bcf", "+79153193112", "hello from c# app");
+        }
+    }
+}

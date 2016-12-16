92236f4a4dc4bd0807d36ee84501608b060dc5d9
Anastasia Berezina <berezinaaa@gmail.com>
2016-12-17 01:19:32 +0400

    add event form

--- UI/EditEventForm.Designer.cs
+++ UI/EditEventForm.Designer.cs
@@ -28,46 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dayPicker = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.priorityComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.telegramCheckBox = new System.Windows.Forms.CheckBox();
+            this.smsCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // titleTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.titleTextBox.Location = new System.Drawing.Point(109, 10);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(200, 20);
+            this.titleTextBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(12, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Название";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 76);
+            this.label2.Location = new System.Drawing.Point(12, 49);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Начало";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Конец";
+            // 
+            // startTimePicker
+            // 
+            this.startTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.startTimePicker.Location = new System.Drawing.Point(109, 36);
+            this.startTimePicker.Name = "startTimePicker";
+            this.startTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.startTimePicker.TabIndex = 9;
+            // 
+            // endTimePicker
+            // 
+            this.endTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.endTimePicker.Location = new System.Drawing.Point(109, 62);
+            this.endTimePicker.Name = "endTimePicker";
+            this.endTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.endTimePicker.TabIndex = 10;
+            // 
+            // dayPicker
+            // 
+            this.dayPicker.Location = new System.Drawing.Point(110, 91);
+            this.dayPicker.Name = "dayPicker";
+            this.dayPicker.Size = new System.Drawing.Size(200, 20);
+            this.dayPicker.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "День";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(109, 117);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(200, 63);
+            this.descriptionTextBox.TabIndex = 13;
+            this.descriptionTextBox.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Описание";
+            // 
+            // priorityComboBox
+            // 
+            this.priorityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.priorityComboBox.FormattingEnabled = true;
+            this.priorityComboBox.Items.AddRange(new object[] {
+            "Низкий",
+            "Обычный",
+            "Высокий"});
+            this.priorityComboBox.Location = new System.Drawing.Point(109, 186);
+            this.priorityComboBox.Name = "priorityComboBox";
+            this.priorityComboBox.Size = new System.Drawing.Size(111, 21);
+            this.priorityComboBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Приоритет";
+            // 
+            // telegramCheckBox
+            // 
+            this.telegramCheckBox.AutoSize = true;
+            this.telegramCheckBox.Location = new System.Drawing.Point(12, 223);
+            this.telegramCheckBox.Name = "telegramCheckBox";
+            this.telegramCheckBox.Size = new System.Drawing.Size(70, 17);
+            this.telegramCheckBox.TabIndex = 17;
+            this.telegramCheckBox.Text = "Telegram";
+            this.telegramCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // smsCheckBox
+            // 
+            this.smsCheckBox.AutoSize = true;
+            this.smsCheckBox.Location = new System.Drawing.Point(12, 246);
+            this.smsCheckBox.Name = "smsCheckBox";
+            this.smsCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.smsCheckBox.TabIndex = 18;
+            this.smsCheckBox.Text = "SMS";
+            this.smsCheckBox.UseVisualStyleBackColor = true;
             // 
             // EditEventForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 365);
+            this.ClientSize = new System.Drawing.Size(333, 365);
+            this.Controls.Add(this.smsCheckBox);
+            this.Controls.Add(this.telegramCheckBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.priorityComboBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.dayPicker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.endTimePicker);
+            this.Controls.Add(this.startTimePicker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.titleTextBox);
             this.Name = "EditEventForm";
             this.Text = "EditEventForm";
+            this.Load += new System.EventHandler(this.EditEventForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,8 +208,20 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox titleTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker startTimePicker;
+        private System.Windows.Forms.DateTimePicker endTimePicker;
+        private System.Windows.Forms.DateTimePicker dayPicker;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox descriptionTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox priorityComboBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox telegramCheckBox;
+        private System.Windows.Forms.CheckBox smsCheckBox;
     }
 }
--- UI/EditEventForm.cs
+++ UI/EditEventForm.cs
@@ -16,5 +16,10 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void EditEventForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
